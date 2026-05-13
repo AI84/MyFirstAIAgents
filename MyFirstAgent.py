6962a0598f6c2ebcdc30720ede29002853290af7
AI84 <148908406+AI84@users.noreply.github.com>
2026-05-13 15:56:05 +0100

    Initial draft  to create agent and assign a LLM model using bedrockModel object

--- MyFirstAgent.py
+++ MyFirstAgent.py
@@ -1,1 +1,16 @@
 
+from strands import Agent
+from strands.models.bedrock import BedrockModel
+
+model = BedrockModel(
+    model_id="amazon.nova-micro-v1:0",
+    region_name="eu-west-2"
+)
+
+agent  = Agent(model = model)
+print("The model  within strands invocation is ", agent.model)
+print(vars(agent.model))
+
+# response = agent("what is the current date and time?")
+
+# print(response)
